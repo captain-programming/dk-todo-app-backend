c8135d3a523df2bdd3fd27f66c483fb16b8acabc
captain-programming <dineshkarde103@gmail.com>
2022-10-28 20:14:06 +0530

    todo-app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const TaskRouter = require("./features/task/task.router");
 const dbConnect = require("./config/db");
 const authMiddleware = require('./middleware/auth.middleware');
 const PORT = process.env.PORT || 8080;
+const cors = require('cors');
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(cors());
 // app.use(authMiddleware);
 app.use("/users", userRouter);
 app.use("/tasks", TaskRouter);
